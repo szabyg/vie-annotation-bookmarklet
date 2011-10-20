5e035cec91a0b5ad94a3a64793cb40de4458c1df
Szaby Grünwald <szaby.gruenwald@salzburgresearch.at>
2011-10-20 20:09:22 +0200

    snml-orf specific event handlers

--- activate.js
+++ activate.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     var appConfig = window.bookmarkletConfig;
     var selector = appConfig.defaultCssSelector;
     if(appConfig.cssPopup){
-        selector = prompt("CSS selector for the annotation (e.g. p, body, div#content...)", appConfig.defaultCssSelector);
+        appConfig.defaultCssSelector = selector = prompt("CSS selector for the annotation (e.g. p, body, div#content...)", appConfig.defaultCssSelector);
     }
+    
+    $("head")[0].insertBefore($("<script>window.bookmarkletConfig = " + JSON.stringify(appConfig) + "</script>")[0], $("head").children()[0]);
     $('#loadingDiv')
     .hide()  // hide it initially
     .ajaxStart(function() {
@@ -12,7 +14,14 @@ $(document).ready(function(){
     .ajaxStop(function() {
         $(this).hide();
     });
-
+    function getResUri() {
+        var duKey = _($("tr")).filter(function(tr){
+            if(tr.children.length === 2 && tr.children[0].textContent.trim() === "DU-Key:"){
+                return true;
+            }
+        })[0].children[1].textContent.trim();
+        return escape(duKey);
+    }
     var z = new VIE();
     z.use(new z.StanbolService({url : appConfig.stanbolUri, proxyDisabled: true}));
     // make the content element editable
@@ -33,6 +42,78 @@ $(document).ready(function(){
         },
         select: function(event, ui){
             console.info('select event', event, ui);
+//            var baseUri = "http://kis28:8080/LMF";
+            var baseUri = "http://labs.newmedialab.at/ORF";
+//            var baseUri = "http://lmf.newmedialab.at/LMF";
+
+            var sparqlUrl = baseUri + "/sparql/update";
+            var resUri = baseUri + "/resource/" + getResUri();
+            var enhancementType = ui.entityEnhancement._enhancement.get("enhancer:entity-type");
+            console.info("enhancement type:", enhancementType);
+            var sparql = "PREFIX dc: <http://purl.org/dc/elements/1.1/> INSERT { <" + 
+                    resUri + "> dc:related  <" +
+                    ui.entityEnhancement.getUri() +
+                    "> } WHERE {}";
+            console.info("sparql command:", sparql);
+            $.ajax({
+                url: sparqlUrl,
+                type: "POST",
+                contentType: "",
+                data: sparql,
+                success: function() {
+                    console.info("sparql update:", arguments);
+                },
+                error: function() {
+                    console.error("sparql update:", arguments);
+                }
+            });
+            function getHtml(){
+                $(":IKS-hallo").trigger("blur");
+                return $("html").html();
+            }
+            function createResource(uri){
+                $.ajax({
+                    url: uri,
+                    type: "POST",
+                    statusCode: {
+                        300: function(){
+                            console.info("300:", arguments);
+                            storeHtml(uri);
+                        },
+                        302: function(){
+                            console.info("302:", arguments);
+                            storeHtml(uri);
+                        }
+                    },
+
+                    success: function() {
+                        console.info("createResource:", arguments);
+                        storeHtml(resUri);
+                    },
+                    error: function() {
+                        console.error("createResource:", arguments);
+                    }
+                });
+            }
+            createResource(resUri);
+            function storeHtml(url){
+                $.ajax({
+                    url: url,
+                    type: "PUT",
+                    data: getHtml(),
+                    beforeSend: function(xhr){
+                        xhr.setRequestHeader("Content-Type", "text/html;rel=content");
+                    },
+                    success: function() {
+                        console.info("storeHtml:", arguments);
+                    },
+                    error: function() {
+                        console.error("storeHtml:", arguments);
+                    }
+                });
+            }
+            console.info("entityEnhancement:", ui.entityEnhancement._enhancement.as("JSON"));
+            console.info("entity uri:", ui.entityEnhancement.getUri());
         },
         remove: function(event, ui){
             console.info('remove event', event, ui);
